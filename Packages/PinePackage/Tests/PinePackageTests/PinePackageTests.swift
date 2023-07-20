9208c350791087ecb78980d483af17efe2c80e6d
tomokisun <tomokisun@users.noreply.github.com>
2023-07-20 05:17:09 +0000

    Run swift-format

--- Packages/PinePackage/Tests/PinePackageTests/PinePackageTests.swift
+++ Packages/PinePackage/Tests/PinePackageTests/PinePackageTests.swift
@@ -1,11 +1,11 @@
-import XCTest
 @testable import PinePackage
+import XCTest
 
 final class PinePackageTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PinePackage().text, "Hello, World!")
-    }
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(PinePackage().text, "Hello, World!")
+  }
 }
