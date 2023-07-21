c665b7a5aeafd013dfdf9ded84c3af0aaf697a19
tomokisun <github@tomokisun.com>
2023-07-21 10:40:57 +0900

    Refactor CornerRadiusBorderButtonStyle to accept a cornerRadius parameter in its initializer

--- Packages/DesignSystemPackage/Sources/ButtonStyles/CornerRadiusBorderButtonStyle.swift
+++ Packages/DesignSystemPackage/Sources/ButtonStyles/CornerRadiusBorderButtonStyle.swift
@@ -1,11 +1,16 @@
 import SwiftUI
 
 public struct CornerRadiusBorderButtonStyle: ButtonStyle {
-  public init() {}
+  let cornerRadius: CGFloat
+  
+  public init(cornerRadius: CGFloat = 16) {
+    self.cornerRadius = cornerRadius
+  }
+  
   public func makeBody(configuration: Configuration) -> some View {
     configuration.label
       .overlay(
-        RoundedRectangle(cornerRadius: 16)
+        RoundedRectangle(cornerRadius: cornerRadius)
           .stroke(Color(uiColor: .separator), lineWidth: 1)
       )
   }
