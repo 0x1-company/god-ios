68eaef532c8c895f85a681bd936c01339158cb0c
tomokisun <github@tomokisun.com>
2023-07-22 16:33:29 +0900

    Add GenderSetting feature with reducer and view

---
+++ Packages/GodPackage/Sources/OnboardFeature/GenderSetting.swift
@@ -0,0 +1,53 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct GenderSettingReducer: ReducerProtocol {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case onTask
+  }
+
+  public var body: some ReducerProtocol<State, Action> {
+    Reduce { _, action in
+      switch action {
+        case .onTask:
+          return .none
+      }
+    }
+  }
+}
+
+public struct GenderSettingView: View {
+  let store: StoreOf<GenderSettingReducer>
+
+  public init(store: StoreOf<GenderSettingReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      List {
+        Text("GenderSetting")
+      }
+      .navigationTitle("GenderSetting")
+      .navigationBarTitleDisplayMode(.inline)
+      .task { await viewStore.send(.onTask).finish() }
+    }
+  }
+}
+
+struct GenderSettingViewPreviews: PreviewProvider {
+  static var previews: some View {
+    GenderSettingView(
+      store: .init(
+        initialState: GenderSettingReducer.State(),
+        reducer: GenderSettingReducer()
+      )
+    )
+  }
+}
