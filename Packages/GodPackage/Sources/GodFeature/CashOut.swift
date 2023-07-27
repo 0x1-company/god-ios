1f1f6352f99faba6114de02e12dcd63a5c4be6b4
tomokisun <github@tomokisun.com>
2023-07-27 21:52:19 +0900

    Add CashOut

---
+++ Packages/GodPackage/Sources/GodFeature/CashOut.swift
@@ -0,0 +1,71 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct CashOutReducer: ReducerProtocol {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case cashOutButtonTapped
+  }
+
+  public var body: some ReducerProtocol<State, Action> {
+    Reduce { _, action in
+      switch action {
+      case .cashOutButtonTapped:
+          return .none
+      }
+    }
+  }
+}
+
+public struct CashOutView: View {
+  let store: StoreOf<CashOutReducer>
+
+  public init(store: StoreOf<CashOutReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      VStack(spacing: 88) {
+        VStack(spacing: 44) {
+          Text("Congrats")
+            .bold()
+            .font(.largeTitle)
+          Text("You earned 9 coins")
+            .bold()
+        }
+        
+        Button {
+          viewStore.send(.cashOutButtonTapped)
+        } label: {
+          Text("Cash Out")
+            .bold()
+            .font(.title2)
+            .frame(height: 54)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(Color.black)
+        }
+        .background(Color.white)
+        .clipShape(Capsule())
+        .shadow(color: .black.opacity(0.2), radius: 25)
+        .padding(.horizontal, 65)
+      }
+    }
+  }
+}
+
+struct CashOutViewPreviews: PreviewProvider {
+  static var previews: some View {
+    CashOutView(
+      store: .init(
+        initialState: CashOutReducer.State(),
+        reducer: CashOutReducer()
+      )
+    )
+  }
+}
