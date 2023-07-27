8c05afbd28054027ce61db10ea3c62483f9f9772
tomokisun <github@tomokisun.com>
2023-07-28 03:37:24 +0900

    Add SceneDelegate

--- Packages/GodPackage/Sources/AppFeature/AppReducer.swift
+++ Packages/GodPackage/Sources/AppFeature/AppReducer.swift
@@ -1,4 +1,5 @@
 import ComposableArchitecture
+import Constants
 import SwiftUI
 
 public struct AppReducer: ReducerProtocol {
@@ -8,22 +9,59 @@ public struct AppReducer: ReducerProtocol {
     public init() {}
 
     var appDelegate = AppDelegateReducer.State()
+    var sceneDelegate = SceneDelegateReducer.State()
   }
 
   public enum Action: Equatable {
     case appDelegate(AppDelegateReducer.Action)
+    case sceneDelegate(SceneDelegateReducer.Action)
+
+    case quickAction(String)
   }
 
   @Dependency(\.openURL) var openURL
+  @Dependency(\.constants) var constants
 
   public var body: some ReducerProtocol<State, Action> {
     Scope(state: \.appDelegate, action: /Action.appDelegate) {
       AppDelegateReducer()
     }
+    Scope(state: \.sceneDelegate, action: /Action.sceneDelegate) {
+      SceneDelegateReducer()
+    }
     Reduce { _, action in
       switch action {
+      case let .appDelegate(.configurationForConnecting(.some(shortcutItem))):
+        let type = shortcutItem.type
+        return .run { send in
+          await send(.quickAction(type))
+        }
+
       case .appDelegate:
         return .none
+
+      case let .sceneDelegate(.shortcutItem(shortcutItem)):
+        let type = shortcutItem.type
+        return .run { send in
+          await send(.quickAction(type))
+        }
+
+      case .sceneDelegate:
+        return .none
+
+      case let .quickAction(key):
+        let urls: [String: URL] = [
+          "talk-to-founder": constants.founderURL(),
+          "talk-to-developer": constants.developerURL(),
+        ]
+
+        guard let url = urls[key] else {
+          return .none
+        }
+
+        return .run { _ in
+          await self.openURL(url)
+        }
       }
     }
   }
@@ -37,8 +75,6 @@ public struct AppView: View {
   }
 
   public var body: some View {
-    NavigationStack {
-      Text("AppView")
-    }
+    Text("AppView")
   }
 }
