4cd7cd9a05af8992dccf88c9e8665c2810b2c759
tomokisun <github@tomokisun.com>
2023-07-28 20:35:37 +0900

    Update God

--- App/God/GodApp.swift
+++ App/God/GodApp.swift
@@ -1,55 +1,74 @@
-import AboutFeature
+import AppFeature
 import ComposableArchitecture
-import GodFeature
-import InboxFeature
-import ProfileFeature
 import SwiftUI
 
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  func windowScene(
+    _ windowScene: UIWindowScene,
+    performActionFor shortcutItem: UIApplicationShortcutItem,
+    completionHandler: @escaping (Bool) -> Void
+  ) {
+    AppDelegate.shared.viewStore.send(.sceneDelegate(.shortcutItem(shortcutItem)))
+    completionHandler(true)
+  }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+  static let shared = AppDelegate()
+  let store = Store(
+    initialState: AppReducer.State(),
+    reducer: AppReducer()._printChanges()
+  )
+
+  var viewStore: ViewStore<Void, AppReducer.Action> {
+    return ViewStore(store.stateless)
+  }
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    viewStore.send(.appDelegate(.didFinishLaunching))
+
+    return true
+  }
+
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    viewStore.send(.appDelegate(.didRegisterForRemoteNotifications(.success(deviceToken))))
+  }
+
+  func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error
+  ) {
+    viewStore.send(.appDelegate(.didRegisterForRemoteNotifications(.failure(error))))
+  }
+
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    viewStore.send(.appDelegate(.configurationForConnecting(options.shortcutItem)))
+    let config = UISceneConfiguration(
+      name: connectingSceneSession.configuration.name,
+      sessionRole: connectingSceneSession.role
+    )
+    config.delegateClass = SceneDelegate.self
+    return config
+  }
+}
+
 @main
-struct GodApp: App {
+struct CaaaptionApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
   var body: some Scene {
     WindowGroup {
-      TabView {
-        InboxView(
-          store: .init(
-            initialState: InboxReducer.State(),
-            reducer: InboxReducer()._printChanges()
-          )
-        )
-        .tabItem {
-          Text("Inbox")
-        }
-
-        QuestionView(
-          store: .init(
-            initialState: QuestionReducer.State(),
-            reducer: QuestionReducer()._printChanges()
-          )
-        )
-        .tabItem {
-          Text("God")
-        }
-
-        ProfileView(
-          store: .init(
-            initialState: ProfileReducer.State(),
-            reducer: ProfileReducer()._printChanges()
-          )
-        )
-        .tabItem {
-          Text("Profile")
-        }
-
-        AboutView(
-          store: .init(
-            initialState: AboutReducer.State(),
-            reducer: AboutReducer()._printChanges()
-          )
-        )
-        .tabItem {
-          Text("About")
-        }
-      }
+      AppView(store: appDelegate.store)
     }
   }
 }
