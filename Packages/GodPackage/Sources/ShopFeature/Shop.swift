69ebbed131ba80907af47f89453218160baeb119
tomokisun <github@tomokisun.com>
2023-07-29 03:31:20 +0900

    Add Shop

--- Packages/GodPackage/Sources/ShopFeature/Shop.swift
+++ Packages/GodPackage/Sources/ShopFeature/Shop.swift
@@ -10,13 +10,20 @@ public struct ShopReducer: ReducerProtocol {
 
   public enum Action: Equatable {
     case onTask
+    case closeButtonTapped
   }
+  
+  @Dependency(\.dismiss) var dismiss
 
   public var body: some ReducerProtocol<State, Action> {
     Reduce { _, action in
       switch action {
       case .onTask:
         return .none
+      case .closeButtonTapped:
+        return .run { _ in
+          await self.dismiss()
+        }
       }
     }
   }
@@ -31,23 +38,71 @@ public struct ShopView: View {
 
   public var body: some View {
     WithViewStore(store, observe: { $0 }) { viewStore in
-      List {
-        Text("Shop")
+      VStack {
+        Text("YOUR BALANCE")
+          .foregroundColor(Color.gray)
+        HStack {
+          Text("68")
+        }
+        .foregroundColor(Color.yellow)
+        
+        Text("Boost Your Name in Polls")
+          .foregroundColor(Color.white)
+        Text("Use coins to get featured in polls")
+          .foregroundColor(Color.gray)
+        
+        VStack {
+          ShopItemView(
+            name: "Get Your Name on 3 Random Polls",
+            description: nil,
+            amount: 100
+          )
+
+          ShopItemView(
+            name: "Put Your Name in Your Crush's Poll",
+            description: "Your name remains secret",
+            amount: 300
+          )
+        }
+        .padding(.horizontal, 16)
+        
+        Spacer()
+        
+        Text("How do I get more coins?")
+          .foregroundColor(Color.white)
+        Text("Answer polls about your friends to win coins.")
+          .foregroundColor(Color.gray)
       }
+      .background(Color.black.gradient)
       .navigationTitle("Shop")
       .navigationBarTitleDisplayMode(.inline)
       .task { await viewStore.send(.onTask).finish() }
+      .toolbar {
+        ToolbarItem(placement: .navigationBarLeading) {
+          Button {
+            viewStore.send(.closeButtonTapped)
+          } label: {
+            Image(systemName: "xmark")
+              .foregroundColor(.primary)
+          }
+        }
+      }
     }
   }
 }
 
 struct ShopViewPreviews: PreviewProvider {
   static var previews: some View {
-    ShopView(
-      store: .init(
-        initialState: ShopReducer.State(),
-        reducer: ShopReducer()
-      )
-    )
+    Text("")
+      .sheet(isPresented: .constant(true)) {
+        NavigationStack {
+          ShopView(
+            store: .init(
+              initialState: ShopReducer.State(),
+              reducer: ShopReducer()
+            )
+          )
+        }
+      }
   }
 }
---
+++ Packages/GodPackage/Sources/ShopFeature/ShopItemView.swift
@@ -0,0 +1,57 @@
+import SwiftUI
+
+struct ShopItemView: View {
+  let name: String
+  let description: String?
+  let amount: Int
+
+  var body: some View {
+    HStack(alignment: .center, spacing: 16) {
+      Color.red
+        .frame(width: 60, height: 60)
+      VStack(spacing: 0) {
+        Text(name)
+          .font(.callout)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .foregroundColor(Color.white)
+
+        if let description = description {
+          Text(description)
+            .font(.caption)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .foregroundColor(Color.gray)
+        }
+      }
+      
+      Button(action: {}) {
+        Text(amount.description)
+          .frame(width: 76, height: 36)
+          .foregroundColor(Color.white)
+          .background(Color.yellow.gradient)
+          .clipShape(Capsule())
+      }
+    }
+    .padding(.horizontal, 16)
+    .frame(height: 96)
+    .background(Color(uiColor: .secondaryLabel))
+    .cornerRadius(16)
+  }
+}
+
+struct ShopItemViewPreviews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ShopItemView(
+        name: "Get Your Name on 3 Random Polls",
+        description: nil,
+        amount: 100
+      )
+      ShopItemView(
+        name: "Put Your Name in Your Crush's Poll",
+        description: "Your name remains secret",
+        amount: 300
+      )
+    }
+    .previewLayout(.sizeThatFits)
+  }
+}
