210b3ddf64a597c6ee9753d8181aa68365c4af37
tomokisun <tomokisun@users.noreply.github.com>
2023-07-28 17:57:34 +0000

    Run swift-format

--- Packages/GodPackage/Sources/GodModeFeature/GodMode.swift
+++ Packages/GodPackage/Sources/GodModeFeature/GodMode.swift
@@ -3,15 +3,15 @@ import SwiftUI
 
 public struct GodModeReducer: ReducerProtocol {
   public init() {}
-  
+
   public struct State: Equatable {
     public init() {}
   }
-  
+
   public enum Action: Equatable {
     case onTask
   }
-  
+
   public var body: some ReducerProtocol<State, Action> {
     Reduce { _, action in
       switch action {
@@ -24,26 +24,26 @@ public struct GodModeReducer: ReducerProtocol {
 
 public struct GodModeView: View {
   let store: StoreOf<GodModeReducer>
-  
+
   public init(store: StoreOf<GodModeReducer>) {
     self.store = store
   }
-  
+
   public var body: some View {
-    WithViewStore(store, observe: { $0 }) { viewStore in
+    WithViewStore(store, observe: { $0 }) { _ in
       VStack(spacing: 24) {
         Text("Recurring billing. Cancel anytime.\nYour payment will be charged to iTunes Account, and your subscription will auto-renew for ¥960/week until you cancel in iTunes Store settings. By tapping Unlock, you agree to our Terms and the auto-renewal.")
           .font(.caption)
           .padding(.horizontal, 24)
           .foregroundColor(Color.gray)
           .multilineTextAlignment(.center)
-        
+
         VStack(spacing: 16) {
           Text("See who likes you")
           Text("with GOD MODE")
-          
+
           Text("¥960/week")
-          
+
           Button(action: {}) {
             Text("Continue")
               .bold()
@@ -54,7 +54,7 @@ public struct GodModeView: View {
               .clipShape(Capsule())
           }
           .padding(.horizontal, 60)
-          
+
           Button(action: {}) {
             Text("Maybe Later")
               .foregroundColor(Color.gray)
@@ -69,7 +69,7 @@ public struct GodModeView: View {
             .stroke(Color.orange, lineWidth: 2)
         )
         .padding(.horizontal, 8)
-        
+
         Spacer()
       }
     }
