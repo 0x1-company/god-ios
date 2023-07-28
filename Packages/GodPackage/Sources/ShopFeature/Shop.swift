dde931e2672344ed8e4e82a9ba8a3dbc81e84328
tomokisun <github@tomokisun.com>
2023-07-29 03:01:20 +0900

    Create ShopFeature

---
+++ Packages/GodPackage/Sources/ShopFeature/Shop.swift
@@ -0,0 +1,53 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct ShopReducer: ReducerProtocol {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case onTask
+  }
+
+  public var body: some ReducerProtocol<State, Action> {
+    Reduce { _, action in
+      switch action {
+        case .onTask:
+          return .none
+      }
+    }
+  }
+}
+
+public struct ShopView: View {
+  let store: StoreOf<ShopReducer>
+
+  public init(store: StoreOf<ShopReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      List {
+        Text("Shop")
+      }
+      .navigationTitle("Shop")
+      .navigationBarTitleDisplayMode(.inline)
+      .task { await viewStore.send(.onTask).finish() }
+    }
+  }
+}
+
+struct ShopViewPreviews: PreviewProvider {
+  static var previews: some View {
+    ShopView(
+      store: .init(
+        initialState: ShopReducer.State(),
+        reducer: ShopReducer()
+      )
+    )
+  }
+}
