8953bfcb5e03b69155d8d1d3f5a7a92572cc814b
tomokisun <github@tomokisun.com>
2023-07-28 20:32:27 +0900

    Add RootNavigation

--- Packages/GodPackage/Sources/NavigationFeature/RootNavigation.swift
+++ Packages/GodPackage/Sources/NavigationFeature/RootNavigation.swift
@@ -1,23 +1,40 @@
 import ComposableArchitecture
 import SwiftUI
+import InboxFeature
+import GodFeature
+import ProfileFeature
+import AboutFeature
 
 public struct RootNavigationReducer: ReducerProtocol {
   public init() {}
   
   public struct State: Equatable {
+    var inbox = InboxReducer.State()
+    var question = QuestionReducer.State()
+    var profile = ProfileReducer.State()
+    var about = AboutReducer.State()
     public init() {}
   }
   
   public enum Action: Equatable {
-    case onTask
+    case inbox(InboxReducer.Action)
+    case question(QuestionReducer.Action)
+    case profile(ProfileReducer.Action)
+    case about(AboutReducer.Action)
   }
   
   public var body: some ReducerProtocol<State, Action> {
-    Reduce { _, action in
-      switch action {
-      case .onTask:
-        return .none
-      }
+    Scope(state: \.inbox, action: /Action.inbox) {
+      InboxReducer()
+    }
+    Scope(state: \.question, action: /Action.question) {
+      QuestionReducer()
+    }
+    Scope(state: \.profile, action: /Action.profile) {
+      ProfileReducer()
+    }
+    Scope(state: \.about, action: /Action.about) {
+      AboutReducer()
     }
   }
 }
@@ -31,12 +48,47 @@ public struct RootNavigationView: View {
   
   public var body: some View {
     WithViewStore(store, observe: { $0 }) { viewStore in
-      List {
-        Text("RootNavigation")
+      TabView {
+        InboxView(
+          store: store.scope(
+            state: \.inbox,
+            action: RootNavigationReducer.Action.inbox
+          )
+        )
+        .tabItem {
+          Text("Inbox")
+        }
+
+        QuestionView(
+          store: store.scope(
+            state: \.question,
+            action: RootNavigationReducer.Action.question
+          )
+        )
+        .tabItem {
+          Text("God")
+        }
+
+        ProfileView(
+          store: store.scope(
+            state: \.profile,
+            action: RootNavigationReducer.Action.profile
+          )
+        )
+        .tabItem {
+          Text("Profile")
+        }
+
+        AboutView(
+          store: store.scope(
+            state: \.about,
+            action: RootNavigationReducer.Action.about
+          )
+        )
+        .tabItem {
+          Text("About")
+        }
       }
-      .navigationTitle("RootNavigation")
-      .navigationBarTitleDisplayMode(.inline)
-      .task { await viewStore.send(.onTask).finish() }
     }
   }
 }
