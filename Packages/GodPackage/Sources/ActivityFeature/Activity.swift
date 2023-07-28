3ed4533f8f0b8f9026a6f68a2f9a9e1f76f7f0a7
tomokisun <github@tomokisun.com>
2023-07-29 03:45:21 +0900

    Activity (#30)

---
+++ Packages/GodPackage/Sources/ActivityFeature/Activity.swift
@@ -0,0 +1,72 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct ActivityReducer: ReducerProtocol {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case onTask
+  }
+
+  public var body: some ReducerProtocol<State, Action> {
+    Reduce { _, action in
+      switch action {
+      case .onTask:
+        return .none
+      }
+    }
+  }
+}
+
+public struct ActivityView: View {
+  let store: StoreOf<ActivityReducer>
+
+  public init(store: StoreOf<ActivityReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      List {
+        ForEach(0..<100, id: \.self) { _ in
+          HStack(alignment: .top, spacing: 16) {
+            Color.red
+              .frame(width: 44, height: 44)
+              .clipShape(Circle())
+            
+            VStack(alignment: .leading, spacing: 4) {
+              HStack(spacing: 0) {
+                Text("Satoya Hatanaka")
+                  .bold()
+                Text(" received")
+              }
+              Text("Harvard would be lucky to have them as a student")
+              Text("From a boy in 9th grade")
+                .foregroundColor(.secondary)
+            }
+            
+            Text("3d")
+              .foregroundColor(.secondary)
+          }
+        }
+      }
+      .listStyle(.plain)
+      .task { await viewStore.send(.onTask).finish() }
+    }
+  }
+}
+
+struct ActivityViewPreviews: PreviewProvider {
+  static var previews: some View {
+    ActivityView(
+      store: .init(
+        initialState: ActivityReducer.State(),
+        reducer: ActivityReducer()
+      )
+    )
+  }
+}
