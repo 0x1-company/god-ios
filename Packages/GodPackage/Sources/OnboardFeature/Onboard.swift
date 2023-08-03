17a3e12f3ca018ddae397621c5c99e3a7624018e
tomokisun <github@tomokisun.com>
2023-08-04 06:59:37 +0900

    Add Onboard navigation

---
+++ Packages/GodPackage/Sources/OnboardFeature/Onboard.swift
@@ -0,0 +1,85 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct OnboardReducer: Reducer {
+  public init() {}
+
+  public struct State: Equatable {
+    public init() {}
+  }
+
+  public enum Action: Equatable {
+    case onTask
+  }
+
+  public var body: some ReducerOf<Self> {
+    Reduce { _, action in
+      switch action {
+      case .onTask:
+        return .none
+      }
+    }
+  }
+}
+
+extension OnboardReducer {
+  public struct Path: Reducer {
+    public enum State: Equatable {
+      case welcome(WelcomeReducer.State = .init())
+      case firstNameSetting(FirstNameSettingReducer.State = .init())
+      case lastNameSetting(LastNameSettingReducer.State = .init())
+      case usernameSetting(UsernameSettingReducer.State = .init())
+      case genderSetting(GenderSettingReducer.State = .init())
+      case avatarSetting(AvatarSettingReducer.State = .init())
+    }
+
+    public enum Action: Equatable {
+      case welcome(WelcomeReducer.Action)
+      case firstNameSetting(FirstNameSettingReducer.Action)
+      case lastNameSetting(LastNameSettingReducer.Action)
+      case usernameSetting(UsernameSettingReducer.Action)
+      case genderSetting(GenderSettingReducer.Action)
+      case avatarSetting(AvatarSettingReducer.Action)
+    }
+
+    public var body: some ReducerOf<Self> {
+      Scope(state: /State.welcome, action: /Action.welcome) {
+        WelcomeReducer()
+      }
+      Scope(state: /State.firstNameSetting, action: /Action.firstNameSetting) {
+        FirstNameSettingReducer()
+      }
+      Scope(state: /State.lastNameSetting, action: /Action.lastNameSetting) {
+        LastNameSettingReducer()
+      }
+      Scope(state: /State.usernameSetting, action: /Action.usernameSetting) {
+        UsernameSettingReducer()
+      }
+      Scope(state: /State.genderSetting, action: /Action.genderSetting) {
+        GenderSettingReducer()
+      }
+      Scope(state: /State.avatarSetting, action: /Action.avatarSetting) {
+        AvatarSettingReducer()
+      }
+    }
+  }
+}
+
+public struct OnboardView: View {
+  let store: StoreOf<OnboardReducer>
+
+  public init(store: StoreOf<OnboardReducer>) {
+    self.store = store
+  }
+
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      List {
+        Text("Onboard")
+      }
+      .navigationTitle("Onboard")
+      .navigationBarTitleDisplayMode(.inline)
+      .task { await viewStore.send(.onTask).finish() }
+    }
+  }
+}
