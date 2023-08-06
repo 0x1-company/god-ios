9b73041b441cf1766fde7249435da5dbbb8bce10
tomokisun <github@tomokisun.com>
2023-08-06 20:39:09 +0900

    Update OneTimeCodeView keyboard type

--- Packages/GodPackage/Sources/OnboardFeature/OneTimeCode.swift
+++ Packages/GodPackage/Sources/OnboardFeature/OneTimeCode.swift
@@ -71,8 +71,8 @@ public struct OneTimeCodeView: View {
             )
           )
           .font(.title)
-          .textContentType(.telephoneNumber)
           .textContentType(.oneTimeCode)
+          .keyboardType(.numberPad)
 
           Spacer()
 
