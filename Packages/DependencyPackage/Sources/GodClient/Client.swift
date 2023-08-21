51012a52fdc5c46a4a4502a5efb55430ea331e6a
tomokisun <github@tomokisun.com>
2023-08-21 12:26:37 +0900

    refactor: 💡 god client

--- Packages/DependencyPackage/Sources/GodClient/Client.swift
+++ Packages/DependencyPackage/Sources/GodClient/Client.swift
@@ -1,5 +1,6 @@
 import God
 
 public struct GodClient {
-  public var updateUsername: (God.UpdateUsernameInput) async throws -> God.UpdateUsernameMutation.Data
+  public var updateUsername: @Sendable (God.UpdateUsernameInput) async throws -> God.UpdateUsernameMutation.Data
+  public var store: @Sendable () -> AsyncThrowingStream<God.StoreQuery.Data, Error>
 }
