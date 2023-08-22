e2f3786a8ba8d56c3192992438e25bd611c88c98
tomokisun <github@tomokisun.com>
2023-08-22 11:48:37 +0900

    refactor: 💡 god client live to use apollo client

--- Packages/DependencyPackage/Sources/GodClient/LiveKey.swift
+++ Packages/DependencyPackage/Sources/GodClient/LiveKey.swift
@@ -4,22 +4,17 @@ import Dependencies
 import Foundation
 import God
 
-public extension ApolloClient {
-  static let god: ApolloClient = {
-    let url = URL(string: "")!
-    return ApolloClient(url: url)
-  }()
-}
-
-extension GodClient: DependencyKey {
-  public static let liveValue = Self(
-    updateUsername: { input in
-      let mutation = God.UpdateUsernameMutation(input: input)
-      return try await ApolloClient.god.perform(mutation: mutation)
-    },
-    store: {
-      let query = God.StoreQuery()
-      return ApolloClient.god.watch(query: query)
-    }
-  )
+extension GodClient {
+  public static func live(apolloClient: @Sendable @escaping () -> ApolloClient) -> Self {
+    return Self(
+      updateUsername: { input in
+        let mutation = God.UpdateUsernameMutation(input: input)
+        return try await apolloClient().perform(mutation: mutation)
+      },
+      store: {
+        let query = God.StoreQuery()
+        return apolloClient().watch(query: query)
+      }
+    )
+  }
 }
