22e76b82a8fd9fbda81d1fb3772954bf1eeb6978
tomokisun <github@tomokisun.com>
2023-09-04 01:22:04 +0900

    feat: 🎸 default selected tab

--- Packages/GodPackage/Sources/NavigationFeature/RootNavigation.swift
+++ Packages/GodPackage/Sources/NavigationFeature/RootNavigation.swift
@@ -10,6 +10,15 @@ import SwiftUI
 
 public struct RootNavigationReducer: Reducer {
   public init() {}
+  
+  public enum Tab: Equatable {
+    case add
+    case activity
+    case inbox
+    case poll
+    case profile
+    case about
+  }
 
   public struct State: Equatable {
     var add = AddReducer.State()
@@ -18,37 +27,28 @@ public struct RootNavigationReducer: Reducer {
     var poll = PollReducer.State()
     var profile = ProfileReducer.State()
     var about = AboutReducer.State()
+    @BindingState var selectedTab = Tab.poll
     public init() {}
   }
 
-  public enum Action: Equatable {
+  public enum Action: Equatable, BindableAction {
     case add(AddReducer.Action)
     case activity(ActivityReducer.Action)
     case inbox(InboxReducer.Action)
     case poll(PollReducer.Action)
     case profile(ProfileReducer.Action)
     case about(AboutReducer.Action)
+    case binding(BindingAction<State>)
   }
 
   public var body: some Reducer<State, Action> {
-    Scope(state: \.add, action: /Action.add) {
-      AddReducer()
-    }
-    Scope(state: \.activity, action: /Action.activity) {
-      ActivityReducer()
-    }
-    Scope(state: \.inbox, action: /Action.inbox) {
-      InboxReducer()
-    }
-    Scope(state: \.poll, action: /Action.poll) {
-      PollReducer()
-    }
-    Scope(state: \.profile, action: /Action.profile) {
-      ProfileReducer()
-    }
-    Scope(state: \.about, action: /Action.about) {
-      AboutReducer()
-    }
+    BindingReducer()
+    Scope(state: \.add, action: /Action.add, child: AddReducer.init)
+    Scope(state: \.activity, action: /Action.activity, child: ActivityReducer.init)
+    Scope(state: \.inbox, action: /Action.inbox, child: InboxReducer.init)
+    Scope(state: \.poll, action: /Action.poll, child: PollReducer.init)
+    Scope(state: \.profile, action: /Action.profile, child: ProfileReducer.init)
+    Scope(state: \.about, action: /Action.about, child: AboutReducer.init)
   }
 }
 
@@ -60,14 +60,15 @@ public struct RootNavigationView: View {
   }
 
   public var body: some View {
-    WithViewStore(store, observe: { $0 }) { _ in
-      TabView {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      TabView(selection: viewStore.$selectedTab) {
         AddView(
           store: store.scope(
             state: \.add,
             action: RootNavigationReducer.Action.add
           )
         )
+        .tag(RootNavigationReducer.Tab.add)
 
         ActivityView(
           store: store.scope(
@@ -75,6 +76,7 @@ public struct RootNavigationView: View {
             action: RootNavigationReducer.Action.activity
           )
         )
+        .tag(RootNavigationReducer.Tab.activity)
 
         InboxView(
           store: store.scope(
@@ -82,6 +84,7 @@ public struct RootNavigationView: View {
             action: RootNavigationReducer.Action.inbox
           )
         )
+        .tag(RootNavigationReducer.Tab.inbox)
 
         PollView(
           store: store.scope(
@@ -89,6 +92,7 @@ public struct RootNavigationView: View {
             action: RootNavigationReducer.Action.poll
           )
         )
+        .tag(RootNavigationReducer.Tab.poll)
 
         ProfileView(
           store: store.scope(
@@ -96,6 +100,7 @@ public struct RootNavigationView: View {
             action: RootNavigationReducer.Action.profile
           )
         )
+        .tag(RootNavigationReducer.Tab.profile)
 
         AboutView(
           store: store.scope(
@@ -103,6 +108,7 @@ public struct RootNavigationView: View {
             action: RootNavigationReducer.Action.about
           )
         )
+        .tag(RootNavigationReducer.Tab.about)
       }
       .ignoresSafeArea()
       .tabViewStyle(.page)
