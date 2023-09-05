ead02b17f2dcb9e0832e7ebe39e9782d25e816be
tomokisun <github@tomokisun.com>
2023-09-05 21:24:13 +0900

    fix: 🐛 change default launch

--- Packages/GodPackage/Sources/AppFeature/AppLogic.swift
+++ Packages/GodPackage/Sources/AppFeature/AppLogic.swift
@@ -22,7 +22,7 @@ public struct AppLogic: Reducer {
 
     var appDelegate = AppDelegateLogic.State()
     var sceneDelegate = SceneDelegateLogic.State()
-    var view = View.State.launch()
+    var view = View.State.onboard()
 
     var quickActionURLs: [String: URL] = [
       "talk-to-founder": Constants.founderURL,
