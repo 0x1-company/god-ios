8765cf466d9418384d6df93eaf7d5a6348cd269c
tomokisun <github@tomokisun.com>
2023-09-14 19:09:33 +0900

    feat: 🎸 add color resource

--- Packages/UIComponentPackage/Sources/Colors/Colors.swift
+++ Packages/UIComponentPackage/Sources/Colors/Colors.swift
@@ -3,19 +3,19 @@ import SwiftUI
 
 public extension Color {
   static let godSeparator = Color(uiColor: UIColor.separator)
-  static let godGray = Color("gray", bundle: .module)
-  static let godBlack = Color("black", bundle: .module)
-  static let godService = Color("service", bundle: .module)
-  static let godTextPrimary = Color("text-primary", bundle: .module)
-  static let godTextSecondaryDark = Color("text-secondary-dark", bundle: .module)
-  static let godTextSecondaryLight = Color("text-secondary-light", bundle: .module)
-  static let godWhite = Color("white", bundle: .module)
-  static let godBackgroundWhite = Color("background-white", bundle: .module)
-  static let godBlue = Color("blue", bundle: .module)
+  static let godGray = Color(ColorResource.gray)
+  static let godBlack = Color(ColorResource.black)
+  static let godService = Color(ColorResource.service)
+  static let godTextPrimary = Color(ColorResource.textPrimary)
+  static let godTextSecondaryDark = Color(ColorResource.textSecondaryDark)
+  static let godTextSecondaryLight = Color(ColorResource.textSecondaryLight)
+  static let godWhite = Color(ColorResource.white)
+  static let godBackgroundWhite = Color(ColorResource.backgroundWhite)
+  static let godBlue = Color(ColorResource.blue)
 
-  static let godPurple = Color("purple", bundle: .module)
-  static let godPurpleLight = Color("purple-light", bundle: .module)
+  static let godPurple = Color(ColorResource.purple)
+  static let godPurpleLight = Color(ColorResource.purpleLight)
 
-  static let godGreen = Color("green", bundle: .module)
-  static let godGreenLight = Color("green-light", bundle: .module)
+  static let godGreen = Color(ColorResource.green)
+  static let godGreenLight = Color(ColorResource.greenLight)
 }
