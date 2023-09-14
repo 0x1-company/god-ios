35d3a5508d5f20ce3bb72baabe21b9184e3f6517
tomokisun <github@tomokisun.com>
2023-09-14 18:02:54 +0900

    feat: 🎸 default localization to en

--- Packages/GodPackage/Package.swift
+++ Packages/GodPackage/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "GodPackage",
+  defaultLocalization: "en",
   platforms: [
     .iOS(.v16),
   ],
--- Packages/UIComponentPackage/Package.swift
+++ Packages/UIComponentPackage/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "UIComponentPackage",
+  defaultLocalization: "en",
   platforms: [
     .iOS(.v16),
   ],
