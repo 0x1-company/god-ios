f2564a5c923d87dce1d6593eb6782f093ef8a197
tomokisun <github@tomokisun.com>
2023-09-26 01:54:46 +0900

    feat: 🎸 analytics screen name

--- Packages/DependencyPackage/Sources/AnalyticsClient/Client.swift
+++ Packages/DependencyPackage/Sources/AnalyticsClient/Client.swift
@@ -1,8 +1,20 @@
+import FirebaseAnalytics
+
 public struct AnalyticsClient {
-  public var logEvent: @Sendable (LogEvent) -> Void
+  public var logEvent: @Sendable (_ name: String, _ parameters: [String: Any]?) -> Void
   public var setUserId: @Sendable (String) -> Void
   public var setUserProperty: @Sendable (_ forName: String, _ value: String?) -> Void
   public var setAnalyticsCollectionEnabled: @Sendable (Bool) -> Void
+}
 
-  public typealias LogEvent = (String, parameters: [String: Any]?)
+public extension AnalyticsClient {
+  func logScreen<T>(screenName: String, of value: T) {
+    logEvent(
+      AnalyticsEventScreenView,
+      [
+        AnalyticsParameterScreenName: screenName,
+        AnalyticsParameterScreenClass: String(describing: type(of: value)),
+      ]
+    )
+  }
 }
