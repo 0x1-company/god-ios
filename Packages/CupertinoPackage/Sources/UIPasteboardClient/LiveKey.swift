3d00908dd7622722aeabd7a151530460715ef786
tomokisun <github@tomokisun.com>
2023-10-01 19:34:47 +0900

    feat: 🎸 paste board

--- Packages/CupertinoPackage/Sources/UIPasteboardClient/LiveKey.swift
+++ Packages/CupertinoPackage/Sources/UIPasteboardClient/LiveKey.swift
@@ -5,6 +5,7 @@ extension UIPasteboardClient: DependencyKey {
   public static let liveValue = Self(
     string: { UIPasteboard.general.string = $0 },
     strings: { UIPasteboard.general.strings = $0 },
+    url: { UIPasteboard.general.url = $0 },
     setItems: { UIPasteboard.general.setItems($0, options: $1) }
   )
 }
--- Packages/CupertinoPackage/Sources/UIPasteboardClient/TestKey.swift
+++ Packages/CupertinoPackage/Sources/UIPasteboardClient/TestKey.swift
@@ -12,6 +12,7 @@ extension UIPasteboardClient: TestDependencyKey {
   public static let testValue = Self(
     string: unimplemented("\(Self.self).string"),
     strings: unimplemented("\(Self.self).strings"),
+    url: unimplemented("\(Self.self).url"),
     setItems: unimplemented("\(Self.self).setItems")
   )
 }
