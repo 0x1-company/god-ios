e2091cd17cff359fd756f1044169f9cc0f27397f
tomokisun <github@tomokisun.com>
2023-10-21 10:30:10 +0900

    feat: 🎸 color

--- Packages/GodPackage/Sources/ProfileEditFeature/ProfileEdit.swift
+++ Packages/GodPackage/Sources/ProfileEditFeature/ProfileEdit.swift
@@ -404,6 +404,7 @@ public struct ProfileEditView: View {
             CornerRadiusBorderButton("Delete Account", systemImage: "trash") {
               store.send(.deleteAccountButtonTapped)
             }
+            .foregroundStyle(Color.red)
           }
         }
         .padding(.all, 24)
