00918e5d2796bb849bbde3da1348f1ede7cced19
tomokisun <github@tomokisun.com>
2023-11-19 04:56:22 +0900

    feat: 🎸 create invite ticket

---
+++ Packages/GodPackage/Sources/ProfileFeature/Sections/InviteTicket.swift
@@ -0,0 +1,22 @@
+import Styleguide
+import SwiftUI
+
+struct InviteTicket: View {
+  let code: String
+  
+  var body: some View {
+    Image(ImageResource.inviteTicket)
+      .resizable()
+      .aspectRatio(contentMode: .fit)
+      .overlay(alignment: .center) {
+        Text(code)
+          .foregroundStyle(Color(0xFFFFCC00))
+          .font(.system(.largeTitle, design: .rounded, weight: .bold))
+          .offset(x: -35, y: 6)
+      }
+  }
+}
+
+#Preview {
+  InviteTicket(code: "ABCDEF")
+}
