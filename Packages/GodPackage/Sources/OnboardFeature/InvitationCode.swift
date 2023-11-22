f009783f5e39ae6f4c0eb77090c28889b3b6a42c
tomokisun <github@tomokisun.com>
2023-11-22 18:22:26 +0900

    feat: 🎸 invitation code

--- Packages/GodPackage/Sources/OnboardFeature/InvitationCode.swift
+++ Packages/GodPackage/Sources/OnboardFeature/InvitationCode.swift
@@ -7,18 +7,29 @@ public struct InvitationCodeLogic {
   public init() {}
 
   public struct State: Equatable {
+    @BindingState var invitationCode = ""
+    var isDisabled = true
     public init() {}
   }
 
-  public enum Action: Equatable {
+  public enum Action: BindableAction {
     case onTask
     case onAppear
+    case nextButtonTapped
+    case skipButtonTapped
+    case binding(BindingAction<State>)
+    case delegate(Delegate)
+
+    public enum Delegate: Equatable {
+      case nextScreen(String?)
+    }
   }
 
   @Dependency(\.analytics) var analytics
 
   public var body: some Reducer<State, Action> {
-    Reduce<State, Action> { _, action in
+    BindingReducer()
+    Reduce<State, Action> { state, action in
       switch action {
       case .onTask:
         return .none
@@ -26,12 +37,27 @@ public struct InvitationCodeLogic {
       case .onAppear:
         analytics.logScreen(screenName: "InvitationCode", of: self)
         return .none
+        
+      case .nextButtonTapped:
+        let code = state.invitationCode
+        return .send(.delegate(.nextScreen(code)))
+        
+      case .skipButtonTapped:
+        return .send(.delegate(.nextScreen(nil)))
+        
+      case .binding(\.$invitationCode):
+        state.isDisabled = state.invitationCode.count != 6
+        return .none
+        
+      default:
+        return .none
       }
     }
   }
 }
 
 public struct InvitationCodeView: View {
+  @FocusState var focus: Bool
   let store: StoreOf<InvitationCodeLogic>
 
   public init(store: StoreOf<InvitationCodeLogic>) {
@@ -40,22 +66,58 @@ public struct InvitationCodeView: View {
 
   public var body: some View {
     WithViewStore(store, observe: { $0 }) { viewStore in
-      List {
-        Text("InvitationCode", bundle: .module)
+      VStack(spacing: 12) {
+        Spacer()
+        Text("Do you have invitation code?", bundle: .module)
+          .font(.system(.title3, design: .rounded, weight: .bold))
+
+        Text("If you do not have it, you can skip it.", bundle: .module)
+          .font(.system(.body, design: .rounded))
+
+        TextField(text: viewStore.$invitationCode) {
+          Text("ABCDEF", bundle: .module)
+        }
+        .foregroundStyle(.white)
+        .multilineTextAlignment(.center)
+        .font(.system(.title, design: .rounded))
+        .focused($focus)
+
+        Spacer()
+
+        NextButton(isDisabled: viewStore.isDisabled) {
+          store.send(.nextButtonTapped)
+        }
+        
+        Button {
+          store.send(.skipButtonTapped)
+        } label: {
+          Text("Skip", bundle: .module)
+            .frame(height: 44)
+            .frame(maxWidth: .infinity)
+            .font(.system(.body, design: .rounded))
+        }
       }
-      .navigationTitle("InvitationCode")
-      .navigationBarTitleDisplayMode(.inline)
+      .padding(.horizontal, 24)
+      .padding(.bottom, 16)
+      .foregroundStyle(Color.white)
+      .background(Color.godService)
       .task { await store.send(.onTask).finish() }
-      .onAppear { store.send(.onAppear) }
+      .onAppear {
+        focus = true
+        store.send(.onAppear)
+      }
     }
   }
 }
 
 #Preview {
-  InvitationCodeView(
-    store: .init(
-      initialState: InvitationCodeLogic.State(),
-      reducer: { InvitationCodeLogic() }
+  NavigationStack {
+    InvitationCodeView(
+      store: .init(
+        initialState: InvitationCodeLogic.State(),
+        reducer: { InvitationCodeLogic() }
+      )
     )
-  )
+  }
+  .environment(\.locale, Locale(identifier: "ja-JP"))
 }
